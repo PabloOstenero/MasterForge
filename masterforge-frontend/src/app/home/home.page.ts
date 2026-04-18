a6f012a032ecac0472c64794a8b420c13dd4ca9c
PabloOstenero <orpablo@iesrafaelalberti.es>
2026-04-18 19:05:13 +0200

    feat: Simple user register page for testing created

--- masterforge-frontend/src/app/home/home.page.ts
+++ masterforge-frontend/src/app/home/home.page.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonAvatar, IonLabel, IonBadge, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonAvatar, IonLabel, IonBadge, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonButton, FormsModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  
+  users: any[] = []; 
+
+  newUser = {
+    name: '',
+    email: ''
+  };
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.apiService.getUsers().subscribe({
+      next: (data) => {
+        console.log('¡Datos recibidos del backend!', data);
+        this.users = data;
+      },
+      error: (err) => {
+        console.error('Error al conectar con el backend', err);
+      }
+    });
+  }
+
+  registerUsers() {
+    // If any of the fields are empty, show an alert and don't proceed
+    if (this.newUser.name === '' || this.newUser.email === '') {
+      alert("Por favor, rellena todos los campos");
+      return;
+    }
+
+    // The backend expects a more complex object, so we need to fill in the rest of the fields behind the scenes. 
+    // Since the form is simple, we can do this easily:
+    const BackendPackage = {
+      name: this.newUser.name,
+      email: this.newUser.email,
+      passwordHash: "123456_temporal", // In the future, you should implement a proper password handling mechanism
+      subscriptionTier: "FREE",
+      balance: 0.00,
+      isActive: true
+    };
+
+    // Call the API to create a new user
+    this.apiService.createUser(BackendPackage).subscribe({
+      next: (ans) => {
+        console.log('¡Usuario creado en la BD!', ans);
+        this.loadUsers(); // Reload the list to show the new user
+        this.newUser = { name: '', email: '' }; // Clean the form
+      },
+      error: (err) => {
+        console.error('Error al crear', err);
+        alert("Hubo un error. ¿A lo mejor ese email ya existe en la base de datos?");
+      }
+    });
+  }
 }
---
+++ masterforge-frontend/src/app/services/api.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  // URL to the Kotlin backend
+  private apiUrl = 'http://localhost:8080/api'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Function to fetch users from the database
+  getUsers(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/users`);
+  }
+
+  // Function to create a new user in the database
+  createUser(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/users`, userData);
+  }
+}
