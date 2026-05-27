5cc78a52c351d548747243ef28ef4a41355d8836
PabloOstenero <orpablo@iesrafaelalberti.es>
2026-05-27 14:27:43 +0200

    refactor: Mobile App name changed to MasterForge

--- masterforge-frontend/capacitor.config.ts
+++ masterforge-frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.masterforge.app',
-  appName: 'masterforge-frontend',
+  appName: 'MasterForge',
   webDir: 'www'
 };
 
