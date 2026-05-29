06bdd371a58403ae1e401c94a20a8c67b08a5a6e
PabloOstenero <orpablo@iesrafaelalberti.es>
2026-05-29 19:48:13 +0200

    ci: fix contextLoads test profile

--- masterforge-backend/src/test/kotlin/com/masterforge/masterforge_backend/MasterforgeBackendApplicationTests.kt
+++ masterforge-backend/src/test/kotlin/com/masterforge/masterforge_backend/MasterforgeBackendApplicationTests.kt
@@ -2,8 +2,10 @@ package com.masterforge.masterforge_backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MasterforgeBackendApplicationTests {
 
 	@Test
